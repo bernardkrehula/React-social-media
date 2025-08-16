e6535c69a8f9e378db758cb4c60c3b787b096f12
Bernard Krehula <krehula.bernard456@gmail.com>
2025-08-16 11:39:20 +0200

    Set data.js

---
+++ my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import data from './data'
+
+function App() {
+
+  return (
+    <>
+      <div className='main'>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ my-app/src/data.js
@@ -0,0 +1,135 @@
+
+const data = {
+    friendsList: [
+        {                  
+            id: crypto.randomUUID(),
+            firstName: "Gordana",
+            lastName: "Stouns",
+            img: "/images/profile1.jpg",
+        },
+        {
+            id: crypto.randomUUID(),
+            firstName: "Hiroshi",
+            lastName: "Tanaka",
+            img: "/images/profile2.jpg",
+        },
+        {
+            id: crypto.randomUUID(),
+            firstName: "Kilibarda",
+            lastName: "Petrovska",
+            img: "/images/profile3.jpg",
+        },
+        {
+            id: crypto.randomUUID(), 
+            firstName: "Majda",
+            lastName: "Odzaklijevska",
+            img: "/images/profile4.jpg",
+        },
+        {
+            id: crypto.randomUUID(),
+            firstName: "Ethan",
+            lastName: "Turner",
+            img: "/images/profile5.jpg",
+        },
+        {
+            id: crypto.randomUUID(),
+            firstName: "Marc",
+            lastName: "Anderson",
+            img: "/images/profile6.jpg",
+        },
+    ],
+    postContentData: [
+        {
+            writenContent: "If I'm to choose between one evil and another, I'd rather not choose at all.",
+            time: "11 months ago",
+
+            postComments: [
+                {
+                    content: "Makes me ponder on the importance of staying true to one's moral compass.",
+                    userName: 'Majda',
+                    userLastName: 'Odzaklijevska',
+                    userImg: '/images/profile4.jpg'
+                },
+                {
+                    content: "It's a reminder that sometimes the best option is to stay true to your principles, even if the choices seem challenging.",
+                    userName: 'Ethan',
+                    userLastName: 'Turner',
+                    userImg: '/images/profile5.jpg'
+                },
+                {
+                    content: "Choosing between evils can be a dilemma, but your stance adds a layer of wisdom to it.",
+                    userName: 'Marc',
+                    userLastName: 'Anderson',
+                    userImg: '/images/profile6.jpg'
+                },
+                {
+                    content: "Sometimes the choices we face are so tough, it's almost like navigating through shades of gray.",
+                    userName: 'Gordana',
+                    userLastName: 'Stouns',
+                    userImg: '/images/profile1.jpg'
+                }
+            ],
+            likes: [
+                {
+                name: "Kilibarda",
+                lastName: "Petrovska",
+                },
+                {
+                    name: "Hiroshi",
+                    lastName: "Tanaka",
+                }
+            ]
+        },
+        {
+            writenContent: "It's not who I am underneath, but what I do that defines me.",
+            time: "about 1 year ago",
+            postComments: [null],
+            likes: [
+                {
+                name: "Kilibarda",
+                lastName: "Petrovska",
+                },
+                {
+                    name: "Hiroshi",
+                    lastName: "Tanaka",
+                }
+            ]
+        },
+        {
+            writenContent: "A true hero isn't measured by the size of his strength but by the strength of his heart",
+            time: "about 1 year ago",
+            postComments: [
+                {
+                    content: "Sometimes, it's the small gestures that make someone a hero",
+                    userName: 'Hiroshi',
+                    userLastName: 'Tanaka',
+                    userImg: '/images/profile4.jpg'
+                },
+                {
+                    content: "Absolutely love this quote!",
+                    userName: 'Marc',
+                    userLastName: 'Anderson',
+                    userImg: '/images/profile6.jpg'
+                },
+                {
+                    content: "Wow, this is so profound and inspiring! Couldn't agree more.",
+                    userName: 'Gordana',
+                    userLastName: 'Stouns',
+                    userImg: '/images/profile1.jpg'
+                }
+            ],
+            likes: [
+                {
+                name: "Kilibarda",
+                lastName: "Petrovska",
+                },
+                {
+                    name: "Hiroshi",
+                    lastName: "Tanaka",
+                }
+            ]
+        }
+    ]
+}
+
+export default data;
