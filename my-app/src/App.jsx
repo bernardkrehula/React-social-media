5d495bd3aed554b54c0cbd5772f779148fecfbab
Bernard Krehula <krehula.bernard456@gmail.com>
2025-08-16 16:36:26 +0200

    Set data as a state

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import data from './data'
 
 function App() {
+  const [ socialMediaData, setSocialMediaData ] = useState({});
 
+  useEffect(() => {
+    setSocialMediaData(data);
+  },[])
+
+  setTimeout(() =>{
+    console.log('radi', socialMediaData)
+  },1000)
   return (
     <>
       <div className='main'>
-        
+        <div className='header'>
+          <input placeholder='🔍 Find friends'/>
+          <img src='/'/>
+        </div>
       </div>
     </>
   )
