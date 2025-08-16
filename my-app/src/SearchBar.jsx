86818535e78e9ade1d653c6b45580fe42a26a5b0
Bernard Krehula <krehula.bernard456@gmail.com>
2025-08-16 17:11:50 +0200

    Filter friends function

--- my-app/src/SearchBar.jsx
+++ my-app/src/SearchBar.jsx
@@ -1,8 +1,12 @@
 
 const SearchBar = ({socialMediaData}) => {
 
+    const handleOnChange = (e) => {
+        const value = e.target.value;
+        const friend = socialMediaData['friendsList'].map(friend => console.log(friend.firstName.toLowerCase().match(value) || friend.lastName.toLowerCase().match(value)))
+    }
     return(
-        <input placeholder='🔍 Find friends'/>
+        <input placeholder='🔍 Find friends' onChange={handleOnChange}/>
     )
 }
 
