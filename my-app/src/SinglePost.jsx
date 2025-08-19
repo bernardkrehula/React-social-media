a66da32bec434b9a6b96087b21edf838da2adbe6
Bernard Krehula <krehula.bernard456@gmail.com>
2025-08-19 22:42:08 +0200

    Style comments

--- my-app/src/SinglePost.jsx
+++ my-app/src/SinglePost.jsx
@@ -11,12 +11,12 @@ const SinglePost = ({initialized, socialMediaData}) => {
     }, [socialMediaData])
 
     return(
-        <div className='post'>
+        <div className='post-section'>
             {postData ? postData.map((data, index) => {
                 const { writenContent, time, postComments, likes } = data;
              
                 return(
-                    <React.Fragment key={index}>
+                    <div className='post' key={index}>
                         <div className='profilePhoto-data'>
                             <img className='profileImg' src='/profilePicture.JPG'/>
                             <div className='profilePhoto-data-text'>
@@ -49,20 +49,21 @@ const SinglePost = ({initialized, socialMediaData}) => {
                             <Btn>Add comment</Btn>
                         </div> 
                         <div className='commentSection'>
-                            {postComments.map((comment, index) => {
+                            {postComments.slice().reverse().map((comment, index) => {
                             const { content, userName, userLastName, userImg} = comment;
                             return(
-                                <div className='comments' key={index}>
+                                <div className='comment' key={index}>
                                     <img src={userImg}/>
-                                    <h2>{userName} {userLastName}</h2>
-                                    <input value={content} disabled/>
+                                    <div className='comment-info'>
+                                        <h2>{userName} {userLastName}</h2>
+                                        <SearchBar placeholder={content}/>
+                                    </div>
+                                    <svg className='dots' xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /></svg>
                                 </div>
                             )
                         })}
                         </div>  
-                         
-                        
-                    </React.Fragment>
+                    </div>
                 )
             }) : ''}
         </div>
