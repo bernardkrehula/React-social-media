6a92c88b529095eb75df78c5e0430870de678b75
Bernard Krehula <krehula.bernard456@gmail.com>
2025-08-19 21:05:02 +0200

    Style post content

--- my-app/src/SinglePost.jsx
+++ my-app/src/SinglePost.jsx
@@ -12,14 +12,16 @@ const SinglePost = ({initialized, socialMediaData}) => {
         <div className='post'>
             {postData ? postData.map((data, index) => {
                 const { writenContent, time, postComments, likes } = data;
-                console.log(postComments)
+             
                 return(
                     <React.Fragment key={index}>
                         <div className='profilePhoto-data'>
-                            <img/>
-                            <h1>Bernard Krehula</h1>
-                            <h2>{time}</h2>
-                            <img/>
+                            <img src='/profilePicture.JPG'/>
+                            <div className='profilePhoto-data-text'>
+                                <h1>Bernard Krehula</h1>
+                                <h2>{time}</h2>
+                            </div>
+                            <svg className='dots' xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /></svg>
                         </div>
                         <p>{writenContent}</p>
                         <div className='comments-tag'>
